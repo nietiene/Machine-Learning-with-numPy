84f76f72d5f3935ab05901cd3a49ac6b7ab617fc
nietiene <niyomugaboetiene53@gmail.com>
2025-08-26 08:55:30 -0700

    this commit contain codes of numPy in python

--- numPy/arrays.py
+++ numPy/arrays.py
@@ -8,7 +8,30 @@ print(type(a)) # print type of a ndarray
 print(a.shape) # print shape of a  3 
 
 # 2D arrays used to represent Dataset, with rows = samples and colums = features 
+# example student marks in three subject
 b = np.array([[1, 2, 3],
              [4, 5, 6]])
 print(b)
 print(b.shape)
+
+# 3D arrays used in DL like images , Audios, Video
+# example color image is stored in 3D (height, width, channel)
+
+c = np.array([[
+    [1, 2], [3, 4],
+    [5, 6], [7, 8]
+]])
+
+print(c)
+print(c.shape)
+
+# Forcing Datatype used in training model that require specific datatype like float32 for neural networks
+d = np.array([1.2, 2.2, 3.4], dtype=np.int32)
+print(d)
+print(d.shape)
+
+# nested arrays:  it is used to slice dataset into row: samples and colums: features
+data = np.array([[10, 20], [30, 40], [50, 60]])
+print(data)
+print(data[0])
+print(data[:,1]) # : means all rows, 1: means on the second column means it fetch all rows in the second column
