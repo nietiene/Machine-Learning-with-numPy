c68b68b7365ce9f5bd9da2acd2723a8c341655e0
nietiene <niyomugaboetiene53@gmail.com>
2025-08-26 08:36:01 -0700

    this commit contain codes of numPy in python

--- numPy/arrays.py
+++ numPy/arrays.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 # np.arrays is function which helps to create arrays in numPy
+# 1D Array think like single feature in ML
 a = np.array([1, 2, 3])
 print(a) # print value of a
 print(type(a)) # print type of a ndarray
 print(a.shape) # print shape of a  3 
+
+# 2D arrays used to represent Dataset, with rows = samples and colums = features 
+b = np.array([[1, 2, 3],
+             [4, 5, 6]])
+print(b)
+print(b.shape)
