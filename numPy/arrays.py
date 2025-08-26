a747db227b8a3d1bb6d7bf3674f53694afb0ac3a
nietiene <niyomugaboetiene53@gmail.com>
2025-08-26 08:31:31 -0700

    this commit contain codes of numPy in python

--- numPy/arrays.py
+++ numPy/arrays.py
@@ -2,4 +2,6 @@ import numpy as np
 
 # np.arrays is function which helps to create arrays in numPy
 a = np.array([1, 2, 3])
-print(a)
+print(a) # print value of a
+print(type(a)) # print type of a ndarray
+print(a.shape) # print shape of a  3 
