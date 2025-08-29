56650a750ac3390daafbe6a2faa7605d4b2cab13
nietiene <niyomugaboetiene53@gmail.com>
2025-08-29 09:24:08 -0700

    simple project makeup by using arrays and boolean indexing  😎

--- numPy/Arrays/miniProject.py
+++ numPy/Arrays/miniProject.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-student_marks = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100]
+student_marks = np.array([45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100])
 print("All marks", student_marks)
 
-above = student_marks[student_marks > 50]
+above = student_marks[student_marks >= 50]
 print("Sutdent above 50%", above)
+
+# top three secores
+top = np.sort(student_marks)[-3:][::-1]
+print("top 3 ", top)
