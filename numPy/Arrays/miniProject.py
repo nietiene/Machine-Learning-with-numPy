42cbb287f34f5db6e81fdb9ab0ec178c7547a94c
nietiene <niyomugaboetiene53@gmail.com>
2025-08-29 09:21:22 -0700

    simple project makeup by using arrays and boolean indexing  😎

---
+++ numPy/Arrays/miniProject.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+student_marks = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100]
+print("All marks", student_marks)
+
+above = student_marks[student_marks > 50]
+print("Sutdent above 50%", above)
