eb345573b92debee9a23fbfccaffff3e39120645
nietiene <niyomugaboetiene53@gmail.com>
2025-08-30 07:46:24 -0700

    elememnt wise-operation commit in python using numPy  😎

--- numPy/Arrays/array3.py
+++ numPy/Arrays/array3.py
@@ -11,4 +11,9 @@ a = np.array([1, 2, 3])
 b = np.array([10 ,20, 30])
 # output = 11, 22, 33
 print(a + b) 
+print(b - a)
+print(a * b)
+print( a/ b)
 
+# usercase: adding bias to the element to bias in neural network + 
+#  error caliculation - , multiplying values to weight
