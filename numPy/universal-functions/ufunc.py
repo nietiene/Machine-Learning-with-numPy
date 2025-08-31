bd49d019ff7a58c96055aabb0f000f450d321eaf
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 08:45:29 -0700

    universal functions (ufnucs) commit in python using numPy  😎

--- numPy/universal-functions/ufunc.py
+++ numPy/universal-functions/ufunc.py
@@ -4,7 +4,7 @@
 
 # 1. np.exp() -> computes an exponential of each element in the input array
 # where e = 2.718 (Euler's number)
-
+# usercase: softmax function  convert secores into probabilities
 import numpy as np
 
 # single value 
@@ -14,3 +14,8 @@ print(np.exp(2)) # e^2
 # array of numbers
 array = np.array([1, 2, 3, 4, 5])
 print(np.exp(array)) # apply exponential to each item in the array
+
+score = np.array([2.0, 5.0, 6.0])
+exp_secore = np.exp(score) # to male all number positive becuase probability can not be less than 0
+probabilites = exp_secore / sum(score)
+print(probabilites)
