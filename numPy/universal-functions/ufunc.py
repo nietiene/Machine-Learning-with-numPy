23d2b831b72a37339e4df475c72ce5e5f1e73693
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 08:57:40 -0700

    universal functions (ufnucs) commit in python using numPy  😎

--- numPy/universal-functions/ufunc.py
+++ numPy/universal-functions/ufunc.py
@@ -15,7 +15,20 @@ print(np.exp(2)) # e^2
 array = np.array([1, 2, 3, 4, 5])
 print(np.exp(array)) # apply exponential to each item in the array
 
+# simple softmax function
 score = np.array([2.0, 5.0, 6.0])
 exp_secore = np.exp(score) # to male all number positive becuase probability can not be less than 0
-probabilites = exp_secore / sum(score)
+probabilites = exp_secore / np.sum(np.exp(score))
 print(probabilites)
+
+# Gradient Descent
+# row output of model
+scores = np.array([1000, 1001, 1002])
+# the why we shifted by max is to make numbers smaller which is better for computation
+shifted_scores = scores - max(scores)
+# make all numbers positive
+exp_secores = np.exp(shifted_scores)
+# find probabilities
+probs = exp_secores / np.sum(exp_secores);
+
+print(probs)
