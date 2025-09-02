ff47d0c5459c1f616b6ffc916a71367ac16f7269
nietiene <niyomugaboetiene53@gmail.com>
2025-09-02 08:59:52 -0700

    aggregations  commit in python using numPy  😎

---
+++ aggregation/aggregation.py
@@ -0,0 +1,23 @@
+
+# ? aggregation means reducing many values into single summary value
+# * example: addding all numbers together, finding maximum and minimum, computing average
+# usercase in ml: summing gradient, caliculating loss, find average, reducing data dimension, 
+
+# 1. np.sum()
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(np.sum(arr)) # print sum of all items
+
+# 2D array
+arr2 = np.array([[1, 2, 3],
+                 [4, 5, 6]]
+                 )
+print(np.sum(arr2))
+
+# loss function caliculating error (difference btn predicted value and true value)
+y_true = np.array([1, 0, 1])
+y_pred = np.array([0.9, 0.2, 0.8])
+
+error = (y_true - y_pred) ** 2
+loss = np.sum(error)
+print(loss)
