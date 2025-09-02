c2d955fceff7c72f9dc143a8d87ded070328bb74
nietiene <niyomugaboetiene53@gmail.com>
2025-09-02 09:13:37 -0700

    ERC20 token with metamask and its frontend

--- aggregation/aggregation.py
+++ aggregation/aggregation.py
@@ -18,6 +18,19 @@ print(np.sum(arr2))
 y_true = np.array([1, 0, 1])
 y_pred = np.array([0.9, 0.2, 0.8])
 
-error = (y_true - y_pred) ** 2
+error = (y_true - y_pred) ** 2 # !square error to avoid negative numbers
 loss = np.sum(error)
 print(loss)
+
+# 2. np.mean() computes average of numbers in the array
+# formula: mean = sum of all elements / number of elements
+mean = np.array([1, 2, 3 ,4, 5, 6, 7, 8])
+#  1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 / 8 = 4.5
+print(np.mean(mean))
+
+# usercase: loss function use averaging error across all samples
+y_true = np.array([2, -0.5, 7])
+y_pred = np.array([1.8, 0.0, 8])
+error = np.mean((y_true - y_pred)  **2)
+
+print(error)
