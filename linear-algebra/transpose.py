4405ec1f74ee66387ee318ed8241e94c23547655
nietiene <niyomugaboetiene53@gmail.com>
2025-09-07 13:22:09 -0700

    python: dot product commit using numpy

---
+++ linear-algebra/transpose.py
@@ -0,0 +1,9 @@
+import numpy as np
+# transpose -> is to transpose matrice by swaps its row and column
+
+A = np.array([[1, 2,],
+              [3, 4]])
+
+print("Original: ", A)
+
+print("Transposed: ", A.T)
