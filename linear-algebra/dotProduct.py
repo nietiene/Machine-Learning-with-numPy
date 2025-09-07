d8b22f195ed3414aa8d83ec155dfe8d9d274334b
nietiene <niyomugaboetiene53@gmail.com>
2025-09-07 10:05:37 -0700

    python: dot product commit using numpy

---
+++ linear-algebra/dotProduct.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# 1. dotProduct: is way of multiplying two vectors -> a1.b1 + a2.b2........
+arr = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+dot = np.dot(arr, arr2)
+print(dot)
