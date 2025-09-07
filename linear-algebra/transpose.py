5b645389d742f6e1f80e4f9dea048cbe858420ea
nietiene <niyomugaboetiene53@gmail.com>
2025-09-07 14:23:11 -0700

    python: transpose &  reshape commit using numpy

--- linear-algebra/transpose.py
+++ linear-algebra/transpose.py
@@ -12,3 +12,13 @@ print("Transposed: ", A.T)
 
 # np.reshape(rows, columns) changes the shape of array without changing its data
 # usercase: in Matrix mulitiplication becuase it require compatible shape
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+print("Reshaped into 2D", arr.reshape(2, 4))
+# -1 tell python I dont know how may column is find it out
+# col = 8 / 2 = 4 so the column will be 4 and two rows
+# useful: when you don't one dimension in advance it caliculate missing dimension
+print("Reshaped into 2D", arr.reshape(2, -1))
+
+# changing 2D into 1D
+print(A.reshape(-1))
+
