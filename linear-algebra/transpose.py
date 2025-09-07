6c193ff5a52b35790e282d8ef56053fd43486d18
nietiene <niyomugaboetiene53@gmail.com>
2025-09-07 14:10:07 -0700

    python: transpose &  reshape commit using numpy

--- linear-algebra/transpose.py
+++ linear-algebra/transpose.py
@@ -1,7 +1,7 @@
 import numpy as np
 # transpose -> is to transpose matrice by swaps its row and column (row become column and column become row)
 # only affect 2D array
-# usercase: is used to align shapes correctly 
+# ? usercase: is used to align shapes correctly 
 
 A = np.array([[1, 2,],
               [3, 4]])
