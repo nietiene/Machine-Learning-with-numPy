b202fcff9e05fffe6dec254b241b89c46d688c4c
nietiene <niyomugaboetiene53@gmail.com>
2025-09-10 12:23:18 -0700

    initial commit of numpy

--- linear-algebra/linearArgebraFunctions.py
+++ linear-algebra/linearArgebraFunctions.py
@@ -1,8 +1,12 @@
 # find inverse of square matrix
+# usercase: in linerar regression when computing with weights using noraml equation
 # ? the inverse of 2 is 1/2
 import numpy as np
 
 A = np.array([[1, 2],
               [4, 5]])
 
-print(np.linalg.inv(A));
+A_inv = np.linalg.inv(A);
+
+print(A @ A_inv);
+
