85aa15c839f47d26aa28b0be720526fb190e2fc6
nietiene <niyomugaboetiene53@gmail.com>
2025-09-11 11:54:56 -0700

    initial commit of numpy

--- linear-algebra/linearArgebraFunctions.py
+++ linear-algebra/linearArgebraFunctions.py
@@ -7,10 +7,13 @@ import numpy as np
 A = np.array([[1, 2],
               [4, 5]])
 
-A_inv = np.linalg.inv(A);
+A_inv = np.linalg.inv(A);9
 
 print(A @ A_inv);
 
 # np.lin.linalg.det () -> determinant is single number that gives you info about matrice
 # measure how it is invertable or scalable
-print(np.linalg.det(A)) 
+print(np.linalg.det(A))
+B = np.array ([[2, 5],
+               [6, 8]])
+print(np.linalg.det(B)) 
